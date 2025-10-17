27c0377e59de53e0cb7072c970a843d4d97634a6
T-r-666 <1154730956@qq.com>
2025-10-17 11:23:13 +0800

    完成不同字符统计的作业

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 input_str = input()
 
-letter_count = 0 
-digit_count = 0   
-space_count = 0  
-other_count = 0   
+# 初始化计数器
+letter_count = 0  # 英文字符
+digit_count = 0   # 数字
+space_count = 0   # 空格
+other_count = 0   # 其他字符
 
 for char in input_str:
+    # 跳过中文字符
     if '\u4e00' <= char <= '\u9fff':
         continue
+        
+    # 统计英文字符（大小写均计为1）
     if ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
         letter_count += 1
+    # 统计数字
     elif '0' <= char <= '9':
         digit_count += 1
+    # 统计空格
     elif char == ' ':
         space_count += 1
+    # 统计其他字符
     else:
         other_count += 1
+
+# 按要求格式输出结果
 print("英文字符: {}".format(letter_count))
 print("数字: {}".format(digit_count))
 print("空格: {}".format(space_count))
