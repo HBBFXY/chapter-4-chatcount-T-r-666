eb04d3cc3dbd5a0c250831f74ce53a336b45b3e3
T-r-666 <1154730956@qq.com>
2025-10-17 10:54:06 +0800

    完成不同字符统计的作业

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-input_str = input("从键盘输入一行字符: ")
-letter_count = 0  
-digit_count = 0  
-space_count = 0  
-other_count = 0   
+input_str=input("从键盘输入一行字符: ")
+letter_count=0  
+digit_count=0  
+space_count=0  
+other_count=0   
 for char in input_str:
     if char.isalpha():
-        letter_count += 1
+        letter_count+=1
     elif char.isdigit():
-        digit_count += 1
+        digit_count+=1
     elif char.isspace():
-        space_count += 1
+        space_count+=1
     else:
-        other_count += 1
+        other_count+=1
 print("英文字符: {}".format(letter_count))
 print("数字: {}".format(digit_count))
 print("空格: {}".format(space_count))
