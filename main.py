d2ade3cab52b506f3fd4dd07bf69c917cbc11c5b
T-r-666 <1154730956@qq.com>
2025-10-17 11:11:59 +0800

    完成不同字符统计的作业

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 input_str = input()
 
-letter_count = 0
-digit_count = 0
-space_count = 0
-other_count = 0
+letter_count = 0  
+digit_count = 0   
+space_count = 0   
+other_count = 0  
 
 for char in input_str:
+    if '\u4e00' <= char <= '\u9fff':
+        continue
+
     if ('a' <= char <= 'z') or ('A' <= char <= 'Z'):
         letter_count += 1
+
     elif '0' <= char <= '9':
         digit_count += 1
+
     elif char == ' ':
         space_count += 1
+
     else:
         other_count += 1
 
@@ -19,3 +25,4 @@ print("英文字符: {}".format(letter_count))
 print("数字: {}".format(digit_count))
 print("空格: {}".format(space_count))
 print("其他字符: {}".format(other_count))
+    
